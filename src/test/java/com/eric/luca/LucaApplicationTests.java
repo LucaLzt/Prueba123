98a10212ed7d2dbc3ba69a98ff208d139bbdd0ed
LucaLzt <lucanlazarte@hotmail.com.ar>
2025-05-17 17:52:04 -0300

    first init

---
+++ src/test/java/com/eric/luca/LucaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.luca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
