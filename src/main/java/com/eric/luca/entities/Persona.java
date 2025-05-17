825af4f192580515e3f69f2926da4ddadef5a4ae
EricM4L4NDR4 <ericboedo01@gmail.com>
2025-05-17 19:28:27 -0300

    Se agregan las clases Cliente y Turno

--- src/main/java/com/eric/luca/entities/Persona.java
+++ src/main/java/com/eric/luca/entities/Persona.java
@@ -4,6 +4,13 @@ public class Persona {
 	private int id;
 	private String nombre;
 	
+	
+	public Persona(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
